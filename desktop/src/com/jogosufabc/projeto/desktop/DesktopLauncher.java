22cb6efdfbcc321f1a33fb8400cf0d7d07c6240b
gsilvap1994 <gsilvap1994@gmail.com>
2018-11-23 21:45:36 -0200

    initial setup

---
+++ desktop/src/com/jogosufabc/projeto/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jogosufabc.projeto.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jogosufabc.projeto.ProjetoFinal;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjetoFinal(), config);
+	}
+}
