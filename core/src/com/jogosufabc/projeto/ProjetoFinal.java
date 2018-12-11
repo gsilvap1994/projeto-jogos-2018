1bc9b7653b4edea489ff96b17a50f0d4bb4f4a8b
Henrique Jötten <henrique_jotten@hotmail.com>
2018-12-11 00:14:20 -0200

    criando outras screens

--- core/src/com/jogosufabc/projeto/ProjetoFinal.java
+++ core/src/com/jogosufabc/projeto/ProjetoFinal.java
@@ -3,19 +3,55 @@ package com.jogosufabc.projeto;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.jogosufabc.projeto.screen.AbstractScreen;
+import com.jogosufabc.projeto.screen.CreditsScreen;
+import com.jogosufabc.projeto.screen.GameScreen;
+import com.jogosufabc.projeto.screen.LoadingScreen;
+import com.jogosufabc.projeto.screen.OptionsScreen;
 import com.jogosufabc.projeto.screen.StartScreen;
 //import com.jogosufabc.projeto.screen.GameScreen;
 
 public class ProjetoFinal extends Game {
 	private AbstractScreen currentScreen;
 	
+	private LoadingScreen loadingScreen;
+	private OptionsScreen optionsScreen;
+	private StartScreen startScreen;
+	private GameScreen gameScreen;
+	private CreditsScreen creditsScreen;
+
+	public final static int MENU = 0;
+	public final static int PREFERENCES = 1;
+	public final static int APPLICATION = 2;
+	public final static int ENDGAME = 3;
+	
 	@Override
     public void create() {
-//		setScreen(new GameScreen());
-		currentScreen = new StartScreen("START");	
+		loadingScreen = new LoadingScreen();
+		setScreen(loadingScreen);
     }
 	
 	public void render() {
 		currentScreen.render(Gdx.graphics.getDeltaTime());
+	}
+	
+	public void changeScreen(int screen){
+		switch(screen){
+			case MENU:
+				if(startScreen == null) startScreen = new StartScreen("START");
+	                        this.setScreen(startScreen);
+				break;
+			case PREFERENCES:
+				if(optionsScreen == null) optionsScreen = new OptionsScreen("OPTIONS");
+				this.setScreen(optionsScreen);
+				break;
+			case APPLICATION:
+				if(gameScreen == null) gameScreen = new GameScreen("GAME");
+				this.setScreen(gameScreen);
+				break;
+			case ENDGAME:
+				if(creditsScreen == null) creditsScreen = new CreditsScreen("CREDITS");
+				this.setScreen(creditsScreen);
+				break;
+		}
 	}
 }
